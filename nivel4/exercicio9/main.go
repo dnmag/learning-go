8231f38885450259070f51c15a66554100f61e69
Daniel Magalhães <magalhaesdaniel2009@gmail.com>
2021-06-08 16:39:56 -0300

    Solves exercise 9 level 4

---
+++ nivel4/exercicio9/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := map[string][]string{
+		"Singer_Bob":      []string{"Caçar monstros", "Beber wisky e vadiar"},
+		"Winchester_Dean": []string{"Caçar monstros", "Beber wisky e vadiar"},
+		"Winchester_Sam":  []string{"Caçar monstros", "Estudar"},
+	}
+
+	x["Winchester_Mary"] = []string{"Caçar monstros", "Matar demonios"}
+
+	for key, v := range x {
+		fmt.Println(key)
+		for i, hobbie := range v {
+			fmt.Printf("\t%v \t%v\n", i, hobbie)
+		}
+	}
+}
